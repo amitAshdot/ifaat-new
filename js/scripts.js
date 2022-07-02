5ab1036032a0e529a2e779ab038b42c06fa8b5ad
Amit Ashdot <amitashdot@gmail.com>
2022-07-02 12:41:25 +0300

    mobile before top section

--- js/scripts.js
+++ js/scripts.js
@@ -166,41 +166,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // -----START CAROUSEL-----
     $('.carousel').slick({
         dots: true,
-        infinite: true,
+        // infinite: true,
         speed: 300,
         slidesToShow: 5.5,
         slidesToScroll: 3,
         // centerPadding: '760px',
-
         // rtl: true,
-        // responsive: [
-        //     {
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             slidesToScroll: 3,
-        //             infinite: true,
-        //             dots: true
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 600,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
-        //         }
-        //     }
-        //     // You can unslick at a given breakpoint now by adding:
-        //     // settings: "unslick"
-        //     // instead of a settings object
-        // ]
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2.5,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
     // -----END CAROUSEL-----
 
